d5351ce909c66f62d2ebb1604e1132c4019e29d1
Wrens <leewrensgerard@gmail.com>
2025-11-27 21:35:45 +0800

    Install Nodemon and finish challenge 1 and 2

--- index.js
+++ index.js
@@ -41,10 +41,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get("/posts", (req,res)=>{
+  res.json(posts);
+});
 
 //CHALLENGE 2: GET a specific post by id
+app.get("/posts/:id", (req,res)=>{
+  const id = parseInt(req.params.id);
+  const idPost = posts.find((post)=>post.id === id);
+  res.json(idPost);
+});
 
 //CHALLENGE 3: POST a new post
+app.post("/api/posts", (req,res)=>{
+
+});
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
 
